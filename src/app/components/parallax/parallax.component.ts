2f6c0dc757ad211bf360f8ae9e5068ed414a992e
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2024-08-08 20:55:21 -0400

    components creation and initial page setup

---
+++ src/app/components/parallax/parallax.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parallax',
+  standalone: true,
+  imports: [],
+  templateUrl: './parallax.component.html',
+  styleUrl: './parallax.component.css'
+})
+export class ParallaxComponent {
+
+}
